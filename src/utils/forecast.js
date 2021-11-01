e62e39355baf0d02a7cb9618fa50cf7c02c570eb
unknown <sa.ov@hotmail.com>
2021-11-01 09:02:17 +0100

    add humidity to src/utils/forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,7 +12,11 @@ request({url,json: true}, (error,{body} = {}) => {
     } else if (body.error) {
         callback('Unable to find location',undefined)
     } else {
-    callback(body.current.weather_descriptions[0]  + '. It is currently ' + body.current.temperature  + ' degrees out.' + ' It feels like ' + body.current.feelslike  + ' degrees out' )
+
+    callback(undefined, body.current.weather_descriptions[0]  + '. It is currently ' + 
+    body.current.temperature  + ' degrees out.' + 
+    ' It feels like ' + body.current.feelslike  + ' degrees out. The humidity is ' + 
+    body.current.humidity + "%.")
         }
 })
 }
